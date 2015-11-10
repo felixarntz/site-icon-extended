22a1c215def8168071aaa396af70b082fe726f6d
Felix Arntz <felix-arntz@t-online.de>
2015-11-10 01:14:51 +0100

    fixed license

--- site-icon-extended.php
+++ site-icon-extended.php
@@ -6,8 +6,8 @@ Description: This plugin enhances the WordPress Site Icon feature by adding more
 Version: 0.2.0
 Author: Felix Arntz
 Author URI: http://leaves-and-love.net
-License: GNU General Public License v2
-License URI: http://www.gnu.org/licenses/gpl-2.0.html
+License: GNU General Public License v3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
 Text Domain: site-icon-extended
 Domain Path: /languages/
 Tags: wordpress, plugin, site-icon, icon, favicon, browser, compatibility, browserconfig, ico, apple-touch-icon, pinned-tab-icon
