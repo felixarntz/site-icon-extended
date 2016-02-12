a00c10d6bd227ddb5bde346a286f29405a5f91d7
Felix Arntz <felix-arntz@t-online.de>
2016-02-12 07:58:20 +0100

    updated dependencies

--- site-icon-extended.php
+++ site-icon-extended.php
@@ -22,19 +22,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die();
 }
 
-if ( ! class_exists( 'WPSIE\App' ) ) {
+if ( version_compare( phpversion(), '5.3.0' ) >= 0 && ! class_exists( 'WPSIE\App' ) ) {
 	if ( file_exists( dirname( __FILE__ ) . '/site-icon-extended/vendor/autoload.php' ) ) {
-		if ( version_compare( phpversion(), '5.3.0' ) >= 0 ) {
-			require_once dirname( __FILE__ ) . '/site-icon-extended/vendor/autoload.php';
-		} else {
-			require_once dirname( __FILE__ ) . '/site-icon-extended/vendor/felixarntz/leavesandlove-wp-plugin-util/leavesandlove-wp-plugin-loader.php';
-		}
+		require_once dirname( __FILE__ ) . '/site-icon-extended/vendor/autoload.php';
 	} elseif ( file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ) {
-		if ( version_compare( phpversion(), '5.3.0' ) >= 0 ) {
-			require_once dirname( __FILE__ ) . '/vendor/autoload.php';
-		} else {
-			require_once dirname( __FILE__ ) . '/vendor/felixarntz/leavesandlove-wp-plugin-util/leavesandlove-wp-plugin-loader.php';
-		}
+		require_once dirname( __FILE__ ) . '/vendor/autoload.php';
+	}
+} elseif ( ! class_exists( 'LaL_WP_Plugin_Loader' ) ) {
+	if ( file_exists( dirname( __FILE__ ) . '/site-icon-extended/vendor/felixarntz/leavesandlove-wp-plugin-util/leavesandlove-wp-plugin-loader.php' ) ) {
+		require_once dirname( __FILE__ ) . '/site-icon-extended/vendor/felixarntz/leavesandlove-wp-plugin-util/leavesandlove-wp-plugin-loader.php';
+	} elseif ( file_exists( dirname( __FILE__ ) . '/vendor/felixarntz/leavesandlove-wp-plugin-util/leavesandlove-wp-plugin-loader.php' ) ) {
+		require_once dirname( __FILE__ ) . '/vendor/felixarntz/leavesandlove-wp-plugin-util/leavesandlove-wp-plugin-loader.php';
 	}
 }
 
